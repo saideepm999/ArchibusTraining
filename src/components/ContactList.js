1c8a5c4245d85fcd76b19a5741abe5d2590f622d
saideep <saideepm@cybage.com>
2022-11-14 16:18:32 +0530

    first commit

---
+++ src/components/ContactList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ContactCard from './ContactCard';
+
+export default function (props) {
+    console.log(props);
+    const renderContactList=props.contacts.map((contacts)=>{
+        return(
+            <ContactCard contacts={contacts}/>
+        )
+    })
+  return (
+    <div className='ui celled list'>
+        {renderContactList}
+
+    </div>
+  )
+}
